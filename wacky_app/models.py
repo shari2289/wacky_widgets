d8a4ec88b5609337ade661bec0be5d6173f57822
Shari Reyes <shari2289@yahoo.com>
2020-09-25 13:36:31 -0500

    Complete Step 3.2

--- wacky_app/models.py
+++ wacky_app/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 class Widget(models.Model):
+    description = models.CharField(max_length=150)
+    quantity = models.IntegerField()
+    
+
+description = models.CharField(max_length=150)
+    quantity = models.IntegerField()
+    def __str__(self):
+        return self.description
+
+    def get_absolute_url(self):
+        return reverse('index')
