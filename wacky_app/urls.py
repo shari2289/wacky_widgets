52e7df21032e804820930a11b6d2543c657140d5
Shari Reyes <shari2289@yahoo.com>
2020-09-25 14:31:27 -0500

    Complete Step 3.4

--- wacky_app/urls.py
+++ wacky_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('widgets/add_widget/', views.add_widget, name='add_widget'),
+    path('widgets/<int:widget_id>/delete/', views.delete_widget, name='delete_widget')
 ]
--- wacky_app/views.py
+++ wacky_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Widget
 from .widget_form import WidgetForm
 
@@ -6,4 +6,14 @@ from .widget_form import WidgetForm
 def index(request):
     widgets = Widget.objects.all()
     widget_form = WidgetForm()
-    return render(request, 'index.html', {'widget_form' : widget_form, 'widgets' : widgets})
+    return render(request, 'index.html', {'widgets': widgets, 'widget_form': widget_form})
+
+def delete_widget(request, widget_id):
+    widget = Widget.objects.get(id=widget_id).delete()
+    return redirect('index')
+
+def add_widget(request):
+  form = WidgetForm(request.POST)
+  if form.is_valid():
+    form.save()
+  return redirect('index')    
